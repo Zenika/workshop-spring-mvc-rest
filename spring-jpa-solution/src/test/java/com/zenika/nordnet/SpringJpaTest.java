8c44e2046da4e31a8267dafde6382df8e48c8ba2
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2012-03-01 11:56:25 +0100

    TP sur JPA + Spring

---
+++ spring-jpa-solution/src/test/java/com/zenika/nordnet/SpringJpaTest.java
@@ -0,0 +1,48 @@
+/**
+ * 
+ */
+package com.zenika.nordnet;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.zenika.nordnet.model.Contact;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration("/spring-jpa.xml")
+public class SpringJpaTest {
+
+	@Autowired private EntityManagerFactory emf;
+	
+	@Test public void springJpa() {
+		EntityManager em = emf.createEntityManager();
+		em.getTransaction().begin();
+		try {
+			Contact contact = new Contact();
+			contact.setFirstname("Mickey");
+			contact.setLastname("Mouse");
+			int initialCount = em.createQuery("from Contact").getResultList().size();
+			em.persist(contact);
+			Assert.assertEquals(
+				initialCount+1,
+				em.createQuery("from Contact").getResultList().size()
+			);
+			em.getTransaction().commit();
+		} finally {
+			em.close();
+		}
+	}
+	
+}
---
+++ spring-jpa-start/src/test/java/com/zenika/nordnet/SpringJpaTest.java
@@ -0,0 +1,26 @@
+/**
+ * 
+ */
+package com.zenika.nordnet;
+
+import javax.persistence.EntityManagerFactory;
+
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+// TODO 04 ajouter les annotations Spring sur le test
+public class SpringJpaTest {
+
+	// TODO 05 déclarer et injecter l'EntityManagerFactory 
+	@Autowired private EntityManagerFactory emf;
+	
+	@Test public void springJpa() {
+		// TODO 06 créer et insérer un Contact
+		// TODO 07 vérifier que le Contact a bien été inséré
+	}
+	
+}
