259a141509640edbdf025f92eeec3f9eacfb7617
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2012-03-07 10:55:08 +0100

    TP spring mvc hello world

---
+++ spring-mvc-hello-world-start/src/main/java/com/zenika/nordnet/web/HelloWorldController.java
@@ -0,0 +1,15 @@
+/**
+ * 
+ */
+package com.zenika.nordnet.web;
+
+
+/**
+ * @author acogoluegnes
+ *
+ */
+// TODO 03 compléter le contrôleur
+// il doit faire afficher "Hello World" pour l'URL /hello
+public class HelloWorldController {
+	
+}
