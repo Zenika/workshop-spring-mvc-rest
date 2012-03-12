35b5a26b015e4b5fa403a4f60cc4abf0cec50564
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2012-03-12 11:42:07 +0100

    relecture

--- spring-data-jpa-requete-jpql-start/src/main/java/com/zenika/nordnet/repository/ContactRepository.java
+++ spring-data-jpa-requete-jpql-start/src/main/java/com/zenika/nordnet/repository/ContactRepository.java
@@ -6,9 +6,7 @@ package com.zenika.nordnet.repository;
 import java.util.List;
 
 import org.springframework.data.repository.Repository;
-import org.springframework.data.repository.query.Param;
 
-import com.zenika.nordnet.model.Address;
 import com.zenika.nordnet.model.Contact;
 
 /**
@@ -31,8 +29,8 @@ public interface ContactRepository extends Repository<Contact,Long> {
 	//   - binder les paramètres de la requête JQPL avec ceux de
 	//     de la méthode Java grâce à une annotation
 	Contact findByFirstnameAndLastname(
-			@Param("firstname") String firstname, 
-			@Param("lastname") String lastname);
+			String firstname, 
+			String lastname);
 
 	// TODO 06 implémenter cette requête en JPQL en tant que requête nommée, sur la classe d'entité
 	// Contraintes :
@@ -40,8 +38,8 @@ public interface ContactRepository extends Repository<Contact,Long> {
 	//   - une seule requête SQL doit être envoyée à la base de données
 	List<Contact> findByAgeGreaterThan(int age);
 	
-	// TODO 08 implémenter cette requête de modification en JPQL
+	// TODO 08 décommenter la méthode ci-dessous et implémenter la requête de modification en JPQL
 	// il s'agit de positionner l'adresse donnée à tous les contacts ayant le nom donné
-	void setNewAddress(Address address,String lastname);
+//	void setNewAddress(Address address,String lastname);
 		
 }
--- spring-data-jpa-tri-pagination-start/src/main/java/com/zenika/nordnet/repository/ContactRepository.java
+++ spring-data-jpa-tri-pagination-start/src/main/java/com/zenika/nordnet/repository/ContactRepository.java
@@ -19,14 +19,14 @@ public interface ContactRepository extends Repository<Contact,Long> {
 	// TODO 01 rendre le tri paramétrable sur cette requête
 	List<Contact> findByLastname(String lastname);
 
-	// TODO 04 implémenter la requête en JPQL et rendre le tri paramétrable
-	List<Contact> findByCity(String city);
+	// TODO 04 décommenter la ligne ci-dessous, implémenter la requête en JPQL et rendre le tri paramétrable
+	// List<Contact> findByCity(String city);
 	
 	// TODO 06 décommenter la ligne ci-dessous, implémenter la requête en JPQL et rendre la méthode paginable
 //	List<Contact> findByCity(String city);
 	
-	// TODO 08 changer la signature de la méthode (paginable, retourne une page au lien d'une List)
+	// TODO 08 décommenter la ligne ci-dessous et changer la signature de la méthode (paginable, retourne une page au lien d'une List)
 	// TODO 09 implémenter la requête en JPQL (fournir une requête pour le count)
-	List<Contact> findPageByCity(String city);
+//	List<Contact> findPageByCity(String city);
 	
 }
