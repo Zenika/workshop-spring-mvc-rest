5cdc35f9289fe9dafa9a3af10558a39e5a9e3488
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2012-06-08 10:11:41 +0200

    packaging

---
+++ package.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+buildDir="./labs-spring-mvc/"
+outputName="labs-spring-mvc"
+
+mvn -f ./pom.xml clean install -Dmaven.test.skip
+mvn -f ./pom.xml dependency:copy-dependencies -Dmdep.useRepositoryLayout=true
+mvn -f ./pom.xml dependency:copy-dependencies -Dclassifier=sources -Dmdep.failOnMissingClassifierArtifact=false -Dmdep.useRepositoryLayout=true
+
+rm -rf $buildDir
+mkdir $buildDir
+mkdir "$buildDir"/repo
+
+for file in $(ls -d ./*/)
+do
+  directory="$file"target/dependency
+  test -e $directory
+#  printf "$file $?\n"
+  if [ "$?" -eq 0 ]
+  then
+    cp -r "$file"target/dependency/* "$buildDir"/repo/
+  fi
+done
+
+mvn -f ./pom.xml clean
+mkdir "$buildDir"/projects
+cp -r ./spring-* "$buildDir"/projects
+cp ./pom.xml "$buildDir"/projects
+cp ./settings.xml "$buildDir"
+mvn -f "$buildDir"/projects/pom.xml eclipse:eclipse
+
+rm -f "$outputName".zip
+zip -qr $outputName "$buildDir"/*
+rm -rf $buildDir
