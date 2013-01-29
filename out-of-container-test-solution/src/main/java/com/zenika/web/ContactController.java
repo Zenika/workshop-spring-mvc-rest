fa61fa8a9f924995b596edf1c355c70c09084c43
Arnaud Cogoluègnes <acogoluegnes@gmail.com>
2013-01-29 14:56:29 +0100

    solution for out-of-container testing

---
+++ out-of-container-test-solution/src/main/java/com/zenika/web/ContactController.java
@@ -0,0 +1,74 @@
+/**
+ * 
+ */
+package com.zenika.web;
+
+import com.zenika.model.Contact;
+import com.zenika.repository.ContactRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+/**
+ * @author acogoluegnes
+ *
+ */
+@Controller
+public class ContactController {
+	
+	@Autowired ContactRepository contactRepository;
+
+	// gestion des erreurs solution 1
+	@RequestMapping(value="/contacts/{id}",method=RequestMethod.GET)
+	@ResponseBody
+	public Contact contact(@PathVariable Long id) {		
+		Contact contact = contactRepository.findOne(id);
+		if(contact == null) {
+			throw new EmptyResultDataAccessException(1);
+		}
+		return contact;
+	}
+	
+	@ExceptionHandler(EmptyResultDataAccessException.class)
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	public void notFound() { }
+	
+	@RequestMapping(value="/contacts",method=RequestMethod.GET)
+	@ResponseBody 
+	public List<Contact> contacts() {
+		return contactRepository.findAll();
+	}
+	
+	@RequestMapping(value="/contacts",method=RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public void create(@RequestBody Contact contact, HttpServletRequest request, HttpServletResponse response) {
+		contact = contactRepository.save(contact);
+		String location = ServletUriComponentsBuilder.fromRequest(request)
+			.pathSegment("{id}")
+			.build()
+			.expand(contact.getId())
+			.encode()
+			.toUriString();
+		response.setHeader("Location", location);
+	}
+	
+	@RequestMapping(value="/contacts/{id}",method=RequestMethod.PUT)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void update(@RequestBody Contact contact) {
+		contactRepository.save(contact);
+	}
+	
+	@RequestMapping(value="/contacts/{id}",method=RequestMethod.DELETE)
+	@ResponseStatus(HttpStatus.NO_CONTENT)
+	public void delete(@PathVariable Long id) {
+		contactRepository.delete(id);
+	}
+	
+}
---
+++ out-of-container-test-solution/src/test/java/com/zenika/web/ContactControllerTest.java
@@ -0,0 +1,144 @@
+package com.zenika.web;
+
+import com.zenika.model.Contact;
+import com.zenika.repository.ContactRepository;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.ArgumentCaptor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
+import org.springframework.web.context.WebApplicationContext;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
+
+/**
+ *
+ *
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration
+@WebAppConfiguration
+public class ContactControllerTest {
+
+    @Autowired
+    WebApplicationContext ctx;
+
+    @Autowired ContactRepository repo;
+
+    MockMvc mockMvc;
+
+    @Before public void setUp() {
+        this.mockMvc = webAppContextSetup(ctx).build();
+        reset(repo);
+    }
+
+    @Test
+    public void contactExists() throws Exception {
+        Long id = 1L;
+        when(repo.findOne(1L)).thenReturn(new Contact(id,"John","Doe",33));
+        mockMvc.perform(get("/contacts/{id}", id))
+//            .andDo(MockMvcResultHandlers.print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("id").value(1))
+            .andExpect(jsonPath("firstname").value("John"))
+            .andExpect(jsonPath("lastname").value("Doe"))
+            .andExpect(jsonPath("age").value(33));
+    }
+
+    @Test
+    public void contactDoesNotExists() throws Exception {
+        Long id = 1L;
+        when(repo.findOne(1L)).thenReturn(null);
+        mockMvc.perform(get("/contacts/{id}", id))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test public void contacts() throws Exception {
+        when(repo.findAll()).thenReturn(Arrays.asList(
+                new Contact(1L,"John","Doe",33),
+                new Contact(2L,"Jane","Doe",30)
+        ));
+        mockMvc.perform(get("/contacts"))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$[0].id").value(1))
+                .andExpect(jsonPath("$[0].firstname").value("John"))
+                .andExpect(jsonPath("$[0].lastname").value("Doe"))
+                .andExpect(jsonPath("$[1].id").value(2))
+                .andExpect(jsonPath("$[1].firstname").value("Jane"))
+                .andExpect(jsonPath("$[1].lastname").value("Doe"));
+    }
+
+    @Test public void create() throws Exception {
+        Contact toBeCreated = new Contact(1L,"John","Doe",33);
+
+        when(repo.save(any(Contact.class))).thenReturn(toBeCreated);
+        mockMvc.perform(post("/contacts")
+                    .content("{\"firstname\":\"John\",\"lastname\":\"Doe\",\"age\":33}")
+                    .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isCreated())
+                .andExpect(header().string("Location","http://localhost/contacts/1"));
+
+        ArgumentCaptor<Contact> contactCaptor = ArgumentCaptor.forClass(Contact.class);
+        verify(repo).save(contactCaptor.capture());
+        Contact captured = contactCaptor.getValue();
+        Assert.assertEquals(toBeCreated.getFirstname(),captured.getFirstname());
+        Assert.assertEquals(toBeCreated.getLastname(),captured.getLastname());
+        Assert.assertEquals(toBeCreated.getAge(),captured.getAge());
+    }
+
+    @Test public void update() throws Exception {
+        Contact toBeUpdated = new Contact(1L,"John","Doe",33);
+
+        when(repo.save(any(Contact.class))).thenReturn(toBeUpdated);
+        mockMvc.perform(put("/contacts/{id}",toBeUpdated.getId())
+                .content("{\"firstname\":\"John\",\"lastname\":\"Doe\",\"age\":33}")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isNoContent());
+
+        ArgumentCaptor<Contact> contactCaptor = ArgumentCaptor.forClass(Contact.class);
+        verify(repo).save(contactCaptor.capture());
+        Contact captured = contactCaptor.getValue();
+        Assert.assertEquals(toBeUpdated.getFirstname(),captured.getFirstname());
+        Assert.assertEquals(toBeUpdated.getLastname(),captured.getLastname());
+        Assert.assertEquals(toBeUpdated.getAge(),captured.getAge());
+    }
+
+    @Test public void deleteContact() throws Exception {
+        mockMvc.perform(delete("/contacts/{id}",1L))
+            .andExpect(status().isNoContent());
+
+        verify(repo).delete(1L);
+    }
+
+
+    @Configuration
+    @EnableWebMvc
+    @ComponentScan(basePackageClasses = ContactController.class)
+    public static class TestConfiguration {
+
+        @Bean
+        public ContactRepository contactRepository() {
+            return mock(ContactRepository.class);
+        }
+
+    }
+
+
+}
